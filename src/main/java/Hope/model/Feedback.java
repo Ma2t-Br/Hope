048d95c6a7f0a9a4482df98fcb0bcce513023ec3
CyrilTakam <127041301+JOJO182003@users.noreply.github.com>
2025-01-16 10:35:15 +0100

    Update Feedback.java

--- src/main/java/Hope/model/Feedback.java
+++ src/main/java/Hope/model/Feedback.java
@@ -1,3 +1,4 @@
+/*
 package Hope.model;
 
 import jakarta.persistence.*;
@@ -80,4 +81,97 @@ public class Feedback {
         this.date = date;
     }
 
+}*/
+
+
+
+package Hope.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.PastOrPresent;
+import org.hibernate.annotations.ColumnDefault;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+@Entity
+@Table(name = "feedback", schema = "hope")
+public class Feedback {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Integer id;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    @JoinColumn(name = "user_id", nullable = false)
+    @NotNull(message = "L'utilisateur associé est obligatoire.")
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    @JoinColumn(name = "tool_id", nullable = false)
+    @NotNull(message = "L'outil associé est obligatoire.")
+    private Tool tool;
+
+    @Lob
+    @Column(name = "commentaire", nullable = false)
+    @NotBlank(message = "Le commentaire ne peut pas être vide.")
+    private String commentaire;
+
+    @ColumnDefault("curdate()")
+    @Column(name = "date")
+    @PastOrPresent(message = "La date doit être dans le passé ou le présent.")
+    private LocalDate date;
+
+    // Getters et Setters
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public Tool getTool() {
+        return tool;
+    }
+
+    public void setTool(Tool tool) {
+        this.tool = tool;
+    }
+
+    public String getCommentaire() {
+        return commentaire;
+    }
+
+    public void setCommentaire(String commentaire) {
+        this.commentaire = commentaire;
+    }
+
+    public String getDate() {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+        LocalDate currentDate = LocalDate.now();
+        if (currentDate.isEqual(date)) {
+            return "Aujourd'hui";
+        } else {
+            return date.format(formatter);
+        }
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
 }
